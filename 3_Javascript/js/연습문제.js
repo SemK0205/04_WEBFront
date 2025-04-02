1a5747ae78613dfc9a47710e32050031bc2dd725
unknown <s413625@gmail.com>
2025-04-02 22:34:16 +0900

    집에서 완료

--- 3_Javascript/js/연습문제.js
+++ 3_Javascript/js/연습문제.js
@@ -17,8 +17,9 @@ document.querySelector("#resNameInput").addEventListener("blur", () => {
 const btnP1 = document.querySelector(".normal-container");
 const btnP2 = document.querySelector(".edit-container");
 
-
+// 기존 메뉴를 input으로 변경
 document.querySelector(".btn-container").addEventListener("click", () => {
+  
   btnP1.classList.add("b-hidden");
   btnP2.classList.remove("b-hidden");
   const spanName = document.querySelectorAll(".menu-name");
@@ -26,112 +27,144 @@ document.querySelector(".btn-container").addEventListener("click", () => {
   const menu = document.querySelector(".menu");
   
   
-  for(let i = 0; i < spanName.length ; i++){
+  for (let i = 0; i < spanName.length; i++) {
     const inputCheck = document.createElement("input");
     const menuContainer = document.querySelector(".menu-container");
     const menuList = document.querySelectorAll(".menu");
+    
+    if(spanName[i].textContent == '미입력'){
+      const menuItem = menuList[i];
+      console.log(spanName[i]);
+      inputCheck.type = "checkbox";
+      inputCheck.classList.add("check");
+
+      const inputName = document.createElement("input");
+      inputName.type = "text";
+      inputName.setAttribute("placeholder", "메뉴명");
+      inputName.classList.add("menu-name-input");
+
+      const inputPrice = document.createElement("input");
+      inputPrice.type = "text";
+      inputPrice.classList.add("menu-price-input");
+      inputPrice.setAttribute("placeholder", "가격");
+
+      menuItem.append(inputCheck, inputName, inputPrice);
+
+      spanPrice[i].remove();
+      spanName[i].remove();
+    } else {
+
     inputCheck.type = "checkbox";
+    inputCheck.classList.add("check");
 
     const inputName = document.createElement("input");
     inputName.type = "text";
-  
     inputName.classList.add("menu-name-input");
-
     inputName.value = spanName[i].textContent;
 
     const inputPrice = document.createElement("input");
     inputPrice.type = "text";
     inputPrice.classList.add("menu-price-input");
-    inputPrice.value = spanPrice[i].textContent;
+    inputPrice.value = spanPrice[i].textContent.replace("원", "");
 
     menuList[i].append(inputCheck);
     menuList[i].append(inputName);
     menuList[i].append(inputPrice);
-    
+
     spanPrice[i].remove();
     spanName[i].remove();
   }
+}
   
 });
 
+// 새로운 메뉴 추가
+document.querySelector("#addMenu").addEventListener("click", newMenu);
 
-document.querySelector("#addMenu").addEventListener("click", () => {
+// 새로운 메뉴 추가 함수(수정 미입력에도 사용할 부분)
+function newMenu() {
+
+    const inputCheck = document.createElement("input");
+    const menuContainer = document.querySelector(".menu-container");
+    inputCheck.type = "checkbox";
+    inputCheck.classList.add("check");
   
-  const inputCheck = document.createElement("input");
-  const menuContainer = document.querySelector(".menu-container");
-  inputCheck.type = "checkbox";
-  inputCheck.classList.add("check");
+    const menuMain = document.createElement("li");
+    menuMain.classList.add("menu");
+  
+    menuContainer.append(menuMain);
+  
+    const inputName = document.createElement("input");
+    inputName.type = "text";
+    inputName.setAttribute("placeholder", "메뉴명");
+    inputName.classList.add("menu-name-input");
+  
+    const inputPrice = document.createElement("input");
+    inputPrice.type = "text";
+    inputPrice.classList.add("menu-price-input");
+    inputPrice.setAttribute("placeholder", "가격");
+  
+    menuMain.append(inputCheck);
+    menuMain.append(inputName);
+    menuMain.append(inputPrice);
+  
+  }
 
-  const menuMain = document.createElement("li");
-  menuMain.classList.add("menu");
 
-  menuContainer.append(menuMain);
-
-  const inputName = document.createElement("input");
-  inputName.type = "text";
-  inputName.setAttribute("placeholder", "메뉴명");
-  inputName.classList.add("menu-name-input");
-
-  const inputPrice = document.createElement("input");
-  inputPrice.type = "text";
-  inputPrice.classList.add("menu-price-input");
-  inputPrice.setAttribute("placeholder", "가격");
-
-  menuMain.append(inputCheck);
-  menuMain.append(inputName);
-  menuMain.append(inputPrice);
-
-});
-
+// 메뉴 삭제
 document.querySelector("#deleteMenu").addEventListener("click", () => {
   const checkedbox = document.querySelectorAll(".check");
-  for(let i =0; i<checkedbox.length; i++){
-    if(checkedbox[i].checked){
+  for (let i = 0; i < checkedbox.length; i++) {
+    if (checkedbox[i].checked) {
       checkedbox[i].parentElement.remove();
     }
   }
 });
 
+// 종료 -> 기존 input 태그를 span태그로 다시 변경 & 입력하지 않았다면 미입력, 0원
 document.querySelector("#exitBtn").addEventListener("click", () => {
   btnP1.classList.remove("b-hidden");
   btnP2.classList.add("b-hidden");
-  
+
+  const menuList = document.querySelectorAll(".menu");
   const inputName = document.querySelectorAll(".menu-name-input");
   const inputPrice = document.querySelectorAll(".menu-price-input");
-  const menu = document.querySelector(".menu");
   const checkedbox = document.querySelectorAll(".check");
-  
-  
-  for(let i = 0; i < inputName.length ; i++){
-    const menuList = document.querySelectorAll(".menu");
-    
+
+
+  for (let i = 0; i < inputName.length; i++) {
+
     const spanName = document.createElement("span");
-    
-    
-    
-    
-    if(inputName[i].value.length == 0){
+
+
+
+
+    if (inputName[i].value.length == 0) {
       spanName.innerText = "미입력";
-    }else {
       spanName.classList.add("menu-name");
-      spanName.innerText = inputName[i].value;
-      
-    }
-    const spanPrice = document.createElement("span");
-    
-    
-    if(inputPrice[i].value.length == 0){
-      spanPrice.innerText = "0원";
-    }else{
-      spanPrice.innerText = inputPrice[i].value;
-      spanPrice.classList.add("menu-price");
+    } else {
+      spanName.classList.add("menu-name");
+      spanName.textContent = inputName[i].value;
 
     }
+    const spanPrice = document.createElement("span");
+
+
+    if (inputPrice[i].value.length == 0) {
+      spanPrice.innerText = "0원";
+      spanPrice.classList.add("menu-price");
+    } else {
+      spanPrice.classList.add("menu-price");
+      spanPrice.textContent = inputPrice[i].value + "원";
+
+    }
+
+    checkedbox[i].remove();
+    inputName[i].remove();
+    inputPrice[i].remove();
 
     menuList[i].append(spanName);
     menuList[i].append(spanPrice);
-    
-    checkedbox[i].parentElement.remove();
   }
 
 
